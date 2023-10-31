bc1af8531fb4786cde2ca26564832411bd1bf601
KJdotIO <98490529+KJdotIO@users.noreply.github.com>
2023-10-31 17:17:27 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [updateCounter, setUpdateCounter] = useState(0)
+
+  const handleCounter = () => {
+    setUpdateCounter(updateCounter + 1)
+  }
+
   return (
-    <h1 className="">
-      Hello world!
-    </h1>
+    <main className="bg-[#191919] max-h-screen">
+      <button onClick={handleCounter} className='bg-white p-10'>{updateCounter}</button>
+    </main>
   );
 }
 
