e4e10207f034bd4550d3239de30477025ec156e5
jjayden22 <jjayden22@icloud.com>
2023-11-01 19:23:49 +0000

    This is a navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import {useState, useEffect} from 'react';
 
+import Navbar from './Components/Navbar'
 function App() {
 
-  const [updateCounter, setUpdateCounter] = useState(0)
-
-  const handleCounter = () => {
-    setUpdateCounter(updateCounter + 1)
-  }
-
   return (
-    <main className="bg-[#191919] max-h-screen">
-      <button onClick={handleCounter} className='bg-white p-10'>{updateCounter}</button>
-    </main>
+
+    <>
+      <Navbar/>
+      <main className="bg-[#191919] max-h-screen">
+       
+      </main>
+
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return(
+        <>
+            <div class="flex-navbar">
+                <div class="logo"> 
+                    <h1>CineWatch.</h1>
+                </div>
+                <div class="buttons">
+                    <button class="signin" type="button">Sign In</button>
+                    <button class="signup"  type="button">Sign Up</button>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Navbar;
