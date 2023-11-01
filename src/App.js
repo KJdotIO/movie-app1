1f3cea954d5f6558187f8f4fb5e45724bf2298ef
enoch-adjei <enochxxiv@gmail.com>
2023-11-01 19:27:09 +0000

    added tagline, description and start button. added transitional animation to button and centered div with flexbox styles.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ function App() {
   }
 
   return (
-    <main className="bg-[#191919] max-h-screen">
-      <button onClick={handleCounter} className='bg-white p-10'>{updateCounter}</button>
+    <main className="bg-[#191919] main-cont">
+      <section class = "main-landing-stuff">
+        <h1 class="tagline">Discover Your New Favourite Film!</h1>
+        <p>Dive into a cinematic journey tailored just for you. Our advanced algorithms match you with movies that resonate with your tastes, ensuring every recommendation is a potential new favourite. No more endless scrolling; just pure cinematic joy.</p>
+        <div class="start-exploring-cont">
+        <button type="button" class="start-exploring">Start Exploring Now!</button>
+        </div>
+      </section>
     </main>
   );
 }
