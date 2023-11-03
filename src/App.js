1edd68d1c8d062c372709326dc6707fc4289b717
jjayden22 <145062461+jm989@users.noreply.github.com>
2023-11-03 14:31:58 +0000

    Navbar changes
    Changed bg colour, padding and font colour of sign in text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <>
             <Navbar/>
-            <main className="bg-#191919 max-h-screen main-cont">
+            <main className="bg-[#191919] max-h-screen main-cont">
                 <section className="main-landing-stuff">
                     <h1 className="tagline">Discover Your New Favourite Film!</h1>
                     <p>Dive into a cinematic journey tailored just for you. Our advanced algorithms match you with movies that resonate with your taste</p>
